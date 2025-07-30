71e4896287ef3d80cbaa65f42389d51871b6e096
Kingsleyaham <kingsleyaham6@gmail.com>
2025-07-30 13:40:42 +0100

    add github action to hubspot app

--- src/app/extensions/config/index.ts
+++ src/app/extensions/config/index.ts
@@ -1,4 +1,6 @@
 export const API_BASE_URL = "https://backend.dev.dealdeck.ai";
+// export const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
+// export const CLIENT_BASE_URL = process.env.REACT_APP_FRONTEND_URL;
 export const CLIENT_BASE_URL = "https://dev.d1i8gtw7s4jtl.amplifyapp.com/en/app";
 // export const CLIENT_BASE_URL = "http://localhost:5173/en/app";
